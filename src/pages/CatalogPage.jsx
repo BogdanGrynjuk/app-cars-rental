36a1c970adfe3c9769f332c788fe416ba737f2a9
BogdanGrynjuk <bgrynjuk@ukr.net>
2024-01-14 20:11:06 +0200

    updates CatalogPage

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,11 +1,20 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectFilteredCars } from "redux/selectors";
 
 import CarsList from "components/CarsList";
 import FilterSection from "components/FilterSection";
+import { resetFilter } from "redux/filtersSlice";
+import { useEffect } from "react";
+
+
 
 const CatalogPage = () => {
-  const filteredCars = useSelector(selectFilteredCars)
+  const filteredCars = useSelector(selectFilteredCars);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(resetFilter());
+  }, [dispatch])
   
   return (
     <>
